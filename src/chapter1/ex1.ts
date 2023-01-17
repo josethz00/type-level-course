a81ad8129133e7cff91578e0f0c4d1dfaece9e07
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-16 22:16:03 -0300

    feat: 1st chapter

--- src/chapter1/ex1.ts
+++ src/chapter1/ex1.ts
@@ -1,16 +1,21 @@
-namespace pipe2 {
-
-  function pipe2<A, B, C>(
-    x: A,
-    f1: (value: A) => B,
-    f2: (value: B) => C
-  ): C {
-    return f2(f1(x));
+/**
+* 1. The `identity` function takes a value of any type
+*    and returns it. Make it generic!
+*/
+export namespace genericFunction {
+  
+  function identity<A>(a: A): A {
+    return a;
   }
-
-  let res1 = pipe2(
-    [1, 2, 3],
-    arr => arr.length,
-    length => `length: ${length}`
-  );
+  
+  let input1 = 10;
+  let res1 = identity(input1);
+  
+  type test1 = Expect<Equal<typeof res1, number>>;
+  
+  let input2 = "Hello";
+  let res2 = identity(input2);
+  
+  type test2 = Expect<Equal<typeof res2, string>>;
+  
 }
