c6958dfb36973fe9f859783c1b747bdd6b008344
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-26 11:26:55 -0300

    feat: chapter2 lessons

---
+++ src/chapter2/ex1.ts
@@ -0,0 +1,22 @@
+/**
+* Type the `move` function so that the `direction`
+* parameter can only be assigned to "backward" or "forward".
+*/
+export namespace move {
+  
+  function move(direction: "backward" | "forward") {
+    // some imaginary code that makes the thing move!
+  }
+  
+  // ✅
+  move("backward")
+  
+  // ✅
+  move("forward")
+  
+  // @ts-expect-error: ❌ not supported
+  move("left")
+  
+  // @ts-expect-error: ❌ not supported
+  move("right")
+}
