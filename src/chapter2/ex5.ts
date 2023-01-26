47fbaca64c880bb626e6dc650eb3c005f78a1525
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-26 11:46:36 -0300

    feat(chapter2): ex5

---
+++ src/chapter2/ex5.ts
@@ -0,0 +1,17 @@
+/**
+* Type the `stringify` function to take any kind of input.
+*
+* Don't use `any`!
+*/
+export namespace stringify {
+  
+  function stringify(input: unknown) {
+    return input instanceof Symbol ? input.toString() : `${input}`;
+  }
+  
+  stringify("a string");    // ✅
+  stringify(12);            // ✅
+  stringify(true);          // ✅
+  stringify(Symbol("cat")); // ✅
+  stringify(20000n);        // ✅
+}
